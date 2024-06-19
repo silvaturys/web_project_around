211f04ebfb33a25046ebfd970a2ac1faf526aa06
silvaturys <fernandes.arthur2704@gmail.com>
2024-06-19 01:18:25 -0300

    adicionar features

--- script/index.js
+++ script/index.js
@@ -40,7 +40,7 @@ formElement.addEventListener("submit", handleProfileFormSubmit);
 
 const addPost = document.querySelector(".profile__add-button");
 const addPostPopup = document.querySelector("#addPostPopup");
-const addClosePopup = document.querySelector("#closeAddPopupButton");
+const addClosePopup = document.querySelector("#CloseAddPopup");
 
 function openAddPost() {
   addPostPopup.classList.add("popup_opened");
@@ -55,20 +55,20 @@ addClosePopup.addEventListener("click", closeAddPost);
 
 
 
-const addPostFormElement = document.querySelector("#addPostForm");
+const addPostFormElement = document.querySelector("#AddPostForm");
 
 function handleAddPostFormSubmit(evt) {
   evt.preventDefault();
 
   const titleInput = document.querySelector("#title");
-  const imageInput = document.querySelector("#image");
+  const linkInput = document.querySelector("#image-link");
 
   const titleValue = titleInput.value;
-  const imageValue = imageInput.value;
+  const linkValue = linkInput.value;
 
   const newCard = {
     name: titleValue,
-    image: imageValue,
+    link: linkValue,
   };
 
   addElements(newCard);
@@ -78,3 +78,87 @@ function handleAddPostFormSubmit(evt) {
 
   closeAddPost();
 }
+
+addPostFormElement.addEventListener("submit", handleAddPostFormSubmit);
+
+const initialCards = [
+  {
+    name: "Vale de Yosemite",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_yosemite.jpg",
+  },
+  {
+    name: "Lago Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lake-louise.jpg",
+  },
+  {
+    name: "Montanhas Carecas",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_latemar.jpg",
+  },
+  {
+    name: "Parque Nacional da Vanoise ",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lago.jpg",
+  },
+];
+
+function addElements(elemento) {
+  const elementsTemplate = document.querySelector("#elements-template").content;
+  const templatePosts = elementsTemplate.cloneNode(true);
+  const elementsPhoto = templatePosts.querySelector(".element__image");
+  const elementsText = templatePosts.querySelector(".element__text");
+
+  elementsPhoto.src = elemento.link;
+  elementsPhoto.alt = elemento.name;
+  elementsText.textContent = elemento.name;
+
+  elementsPhoto.addEventListener("click", (evt) => openImage(evt));
+
+  const likeButtonActive = templatePosts.querySelector(".element__icon");
+  likeButtonActive.addEventListener("click", function (evt) {
+    evt.target.classList.toggle("elements__icon-active");
+  });
+
+  const deletePost = templatePosts.querySelector(".element__icon-trash");
+
+  deletePost.addEventListener("click", function () {
+    const removePost = deletePost.closest(".element");
+    removePost.remove();
+  });
+
+  const elementsList = document.querySelector(".elements");
+  elementsList.prepend(templatePosts);
+}
+
+initialCards.forEach(addElements);
+
+const popupImage = document.querySelector(".popup__image");
+const popupParagraph = document.querySelector(".popup__paragraph");
+const openPopupImage = document.querySelector("#PopupImage");
+const closePopupImage = document.querySelector("#CloseImagePopup");
+
+//funções open image
+
+function openImage(evt) {
+  const card = evt.target.offsetParent;
+  const image = card.querySelector(".element__image");
+  const paragraph = card.querySelector(".element__text");
+  popupImage.src = image.src;
+  popupImage.alt = image.alt;
+  popupParagraph.textContent = paragraph.textContent;
+  openPopupImage.classList.add("popup__image_opened");
+}
+
+function closeImage() {
+  openPopupImage.classList.remove("popup__image_opened");
+}
+
+//eventos open image
+
+closePopupImage.addEventListener("click", closeImage);
